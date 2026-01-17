2c0ee02e35a30a8bd41fd8fc97bc00d777a1dd62
Mike Will <myQwil@gmail.com>
2026-01-17 01:12:45 -0500

    build for windows and macos

--- build.zig
+++ build.zig
@@ -14,72 +14,92 @@ const Options = struct {
 	}
 };
 
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
 	const target = b.standardTargetOptions(.{});
 	const optimize = b.standardOptimizeOption(.{});
 	const opt: Options = .init(b);
 
 	//---------------------------------------------------------------------------
 	// Library
-	const lib = blk: {
-		const upstream = b.dependency("rubberband", .{});
+	const upstream = b.dependency("rubberband", .{});
+	const mod = b.createModule(.{
+		.target = target,
+		.optimize = optimize,
+		.link_libcpp = true,
+	});
 
-		const mod = b.createModule(.{
-			.target = target,
-			.optimize = optimize,
-			.link_libcpp = true,
-		});
-		mod.addCSourceFiles(.{
-			.root = upstream.path("src"),
-			.files = &.{
-				"rubberband-c.cpp",
-				"RubberBandStretcher.cpp",
-				"RubberBandLiveShifter.cpp",
-				"faster/AudioCurveCalculator.cpp",
-				"faster/CompoundAudioCurve.cpp",
-				"faster/HighFrequencyAudioCurve.cpp",
-				"faster/SilentAudioCurve.cpp",
-				"faster/PercussiveAudioCurve.cpp",
-				"faster/R2Stretcher.cpp",
-				"faster/StretcherChannelData.cpp",
-				"faster/StretcherProcess.cpp",
-				"common/Allocators.cpp",
-				"common/FFT.cpp",
-				"common/Log.cpp",
-				"common/Profiler.cpp",
-				"common/Resampler.cpp",
-				"common/StretchCalculator.cpp",
-				"common/sysutils.cpp",
-				"common/mathmisc.cpp",
-				"common/Thread.cpp",
-				"finer/R3Stretcher.cpp",
-				"finer/R3LiveShifter.cpp",
-				"common/BQResampler.cpp",
-			},
-			.flags = &.{
-				// "-DHAVE_LIBSAMPLERATE",
-				"-DUSE_BQRESAMPLER",
-				"-DUSE_BUILTIN_FFT",
-				"-DUSE_PTHREADS",
-				"-DHAVE_POSIX_MEMALIGN",
-			},
-		});
+	const mem = b.allocator;
+	const os = target.result.os.tag;
 
-		const lib = b.addLibrary(.{
-			.name = "rubberband",
-			.linkage = opt.linkage,
-			.root_module = mod,
+	var files: std.ArrayList([]const u8) = .{};
+	defer files.deinit(mem);
+
+	try files.appendSlice(mem, &.{
+		"rubberband-c.cpp",
+		"RubberBandStretcher.cpp",
+		"RubberBandLiveShifter.cpp",
+		"faster/AudioCurveCalculator.cpp",
+		"faster/CompoundAudioCurve.cpp",
+		"faster/HighFrequencyAudioCurve.cpp",
+		"faster/SilentAudioCurve.cpp",
+		"faster/PercussiveAudioCurve.cpp",
+		"faster/R2Stretcher.cpp",
+		"faster/StretcherChannelData.cpp",
+		"faster/StretcherProcess.cpp",
+		"common/Allocators.cpp",
+		"common/FFT.cpp",
+		"common/Log.cpp",
+		"common/Profiler.cpp",
+		"common/Resampler.cpp",
+		"common/StretchCalculator.cpp",
+		"common/sysutils.cpp",
+		"common/mathmisc.cpp",
+		"common/Thread.cpp",
+		"finer/R3Stretcher.cpp",
+		"finer/R3LiveShifter.cpp",
+		"common/BQResampler.cpp",
+	});
+
+	if (os.isDarwin()) {
+		mod.addCMacro("USE_PTHREADS", "1");
+		mod.addCMacro("MALLOC_IS_ALIGNED", "1");
+		mod.addCMacro("LACK_SINCOS", "1");
+	} else if (os == .windows) {
+		mod.addCMacro("_WIN32", "1");
+		mod.addCMacro("NOMINMAX", "1");
+		mod.addCMacro("_USE_MATH_DEFINES", "1");
+		mod.addCMacro("GETOPT_API", "");
+		try files.appendSlice(mem, &.{
+			"ext/getopt/getopt.c",
+			"ext/getopt/getopt_long.c",
 		});
-		lib.installHeadersDirectory(upstream.path("rubberband"), "", .{
-			.include_extensions = &.{
-				"rubberband-c.h",
-				"RubberBandStretcher.h",
-				"RubberBandLiveShifter.h",
-			},
-		});
-		b.installArtifact(lib);
-		break :blk lib;
-	};
+	} else if (os == .linux) {
+		mod.addCMacro("USE_PTHREADS", "1");
+		mod.addCMacro("HAVE_POSIX_MEMALIGN", "1");
+	}
+
+	mod.addCMacro("USE_BUILTIN_FFT", "1");
+	mod.addCMacro("USE_BQRESAMPLER", "1");
+
+	mod.addCSourceFiles(.{
+		.root = upstream.path("src"),
+		.files = files.items,
+		.flags = &.{},
+	});
+
+	const lib = b.addLibrary(.{
+		.name = "rubberband",
+		.linkage = opt.linkage,
+		.root_module = mod,
+	});
+	lib.installHeadersDirectory(upstream.path("rubberband"), "", .{
+		.include_extensions = &.{
+			"rubberband-c.h",
+			"RubberBandStretcher.h",
+			"RubberBandLiveShifter.h",
+		},
+	});
+	b.installArtifact(lib);
 
 	//---------------------------------------------------------------------------
 	// Zig module
